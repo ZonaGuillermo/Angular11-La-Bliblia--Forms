8fd70a5f0b9e7bf299fafbb7e87e57597963a02a
ZonaGuillermo <zonaguillermoweb@gmail.com>
2022-06-27 20:22:49 +0200

    Section 8: Forms (video 89 viewed)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './components/template/template.component';
+import { ModelComponent } from './components/model/model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    ModelComponent
   ],
   imports: [
     BrowserModule,
